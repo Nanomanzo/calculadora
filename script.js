1fb660a70351f1d27ea832b779fc58d2c1634a11
Alejandro Manzo <nanommfp@gmail.com>
2022-08-05 18:36:17 +0200

    funciones para agregar los numeros al arrayPrimerNumeroListo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let input = document.querySelector("inputOperaciones");
+let input = document.querySelector(".inputOperaciones");
 let eliminar = document.querySelector(".eliminar");
 let borrar = document.querySelector(".borrar");
 let dividir = document.querySelector(".dividir");
@@ -17,4 +17,81 @@ let sumar = document.querySelector(".sumar");
 let numero0 = document.querySelector(".cero");
 let decimal = document.querySelector(".decimal");
 let contendorResultados = document.querySelector(".contendorResultados");
+
+let arryPrimerNumero =[];
+let arrySegundoNumero = [];
+
+numero0.addEventListener ("click", agregarNumero0);
+numero1.addEventListener ("click", agregarNumero1);
+numero2.addEventListener ("click", agregarNumero2);
+numero3.addEventListener ("click", agregarNumero3);
+numero4.addEventListener ("click", agregarNumero4);
+numero5.addEventListener ("click", agregarNumero5);
+numero6.addEventListener ("click", agregarNumero6);
+numero7.addEventListener ("click", agregarNumero7);
+numero8.addEventListener ("click", agregarNumero8);
+numero9.addEventListener ("click", agregarNumero9);
+
+
+
+function agregarNumero0() {
+    arryPrimerNumero.push("0");
+    console.log(arryPrimerNumero);
+    input.value = arryPrimerNumero;
+}
+
+function agregarNumero1() {
+    arryPrimerNumero.push("1");
+    console.log(arryPrimerNumero);
+    input.value = arryPrimerNumero;
+}
+
+function agregarNumero2() {
+    arryPrimerNumero.push("2");
+    console.log(arryPrimerNumero);
+    input.value = arryPrimerNumero;
+}
+
+function agregarNumero3() {
+    arryPrimerNumero.push("3");
+    console.log(arryPrimerNumero);
+    input.value = arryPrimerNumero;
+}
+
+function agregarNumero4() {
+    arryPrimerNumero.push("4");
+    console.log(arryPrimerNumero);
+    input.value = arryPrimerNumero;
+}
+
+function agregarNumero5() {
+    arryPrimerNumero.push("5");
+    console.log(arryPrimerNumero);
+    input.value = arryPrimerNumero;
+}
+
+function agregarNumero6() {
+    arryPrimerNumero.push("6");
+    console.log(arryPrimerNumero);
+    input.value = arryPrimerNumero;
+}
+
+function agregarNumero7() {
+    arryPrimerNumero.push("7");
+    console.log(arryPrimerNumero);
+    input.value = arryPrimerNumero;
+}
+
+function agregarNumero8() {
+    arryPrimerNumero.push("8");
+    console.log(arryPrimerNumero);
+    input.value = arryPrimerNumero;
+}
+
+function agregarNumero9() {
+    arryPrimerNumero.push("9");
+    console.log(arryPrimerNumero);
+    input.value = arryPrimerNumero;
+}
+
 
